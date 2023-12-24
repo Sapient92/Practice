31542b60e4ef45d5ccd1fa83dbd564c3c25772bb
Sapient92 <>
2023-12-24 18:17:12 +0900

    react로 todo 앱 만들기(tailwind CSS 사용)

---
+++ react-todo-practice/src/App.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useState } from "react";
+import "./App.css";
+import Lists from "./Components/Lists";
+import Form from "./Components/Form";
+
+const initialTodoData = localStorage.getItem("todoData")
+  ? JSON.parse(localStorage.getItem("todoData"))
+  : [];
+
+export default function App() {
+  const [todoData, setTodoData] = useState(initialTodoData);
+  const [value, setValue] = useState("");
+
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    },
+    [todoData]
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+
+    setTodoData((prev) => [...prev, newTodo]);
+    setValue("");
+    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]));
+  };
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem("todoData", JSON.stringify([]));
+  };
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
+        </div>
+        <Lists
+          todoData={todoData}
+          setTodoData={setTodoData}
+          handleClick={handleClick}
+        />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+      </div>
+    </div>
+  );
+}
