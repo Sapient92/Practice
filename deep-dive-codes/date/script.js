2c26f582c2d32be58a8d339682ebe7653c9ecd87
Sapient92 <151002897+Sapient92@users.noreply.github.com>
2024-01-19 10:09:48 +0900

    Delete deep-dive-codes directory

--- deep-dive-codes/date/script.js
+++
@@ -1,8 +0,0 @@
-// const target = "color colour colouur";
-// const regExp = /colou?r/g;
-
-// console.log(target.match(regExp));
-
-// const target = "123a45";
-// const regExp = /^\d+$/;
-// console.log(regExp.test(target));
