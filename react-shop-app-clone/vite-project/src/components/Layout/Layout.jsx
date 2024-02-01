37c5087e9d424a09e066c466594b156cb326c07d
Sapient92 <angels6312@naver.com>
2024-02-01 21:33:38 +0900

    shop-app clone

---
+++ react-shop-app-clone/vite-project/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import {Outlet} from "react-router-dom";
+import Footer from "../footer/Footer";
+import Header from "../header/header";
+import styles from "./Layout.module.scss";
+const Layout = () => {
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
