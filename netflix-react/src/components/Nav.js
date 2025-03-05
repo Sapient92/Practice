5240ea70b4154599478813f1e3ee418898ee9527
Sapient92 <angels6312@naver.com>
2025-03-05 14:08:35 +0900

    install styled components

--- netflix-react/src/components/Nav.js
+++ netflix-react/src/components/Nav.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import "./Nav.css";
+
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
-    <nav>
-      <img alt="Netflex logo" />
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflex logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+        className="nav__avatar"
+      />
     </nav>
   );
 };
